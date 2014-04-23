187e577ead89e97669b96ca7625c6e3695c88c8c
unknown <A@.(none)>
2014-04-23 18:03:15 +0900

    2014-04-24 pm6:03

---
+++ rst_bingo/routes/main.js
@@ -0,0 +1,37 @@
+//2014-04-24 p.245 추가
+var bingo = {
+	is_my_turn: Boolean,
+	socket: null,
+	
+	init: function (obj) {
+				
+	},
+	
+	select_num: function (num){
+				
+	},
+	
+	where_is_it: function(obj){
+		
+	},
+	
+	chek_num: function(obj){
+		
+	},
+	
+	update_userlist: function(data){
+		
+	},
+	
+	leave: function(){
+		
+	},
+	
+	print_msg: function(msg){
+		
+	}
+};
+
+$(document).ready(function(){
+	bingo.init();	
+});

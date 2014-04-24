8c7b75f8ca97805f821b5a8649b8cd9580be119d
unknown <byeong_gyu@naver.com>
2014-04-24 16:56:30 +0900

    test

---
+++ rst_bingo/public/javascripts/main.js
@@ -0,0 +1,37 @@
+//2014-04-24 p.245 추가
+var bingo = {
+        is_my_turn: Boolean,
+        socket: null,
+
+        init: function () {
+
+        },
+
+        select_num: function (obj){
+
+        },
+
+        where_is_it: function(num){
+
+        },
+
+        chek_num: function(obj){
+
+        },
+
+        update_userlist: function(data){
+
+        },
+
+        leave: function(){
+
+        },
+
+        print_msg: function(msg){
+
+        }
+};
+
+$(document).ready(function(){
+        bingo.init();
+});

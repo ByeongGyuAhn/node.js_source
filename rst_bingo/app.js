d568d1364d2bdf0fc5b733a7e3ee5fa69748e5d2
unknown <byeong_gyu@naver.com>
2014-04-25 15:59:23 +0900

    2014-04-25 pm3:58 app.js /javascript/main.js source_update

--- rst_bingo/app.js
+++ rst_bingo/app.js
@@ -28,6 +28,66 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/main', routes.main);
-app.get('users', user.list);
+app.get('/users', user.list);
+
+var server = http.createServer(app);
+
+var io = socketio.listen(server);
+
+server.listen(app.get('port'), function(){
+    console.log("express server listening on port" + app.get('port'));
+});
+
+var user = {};
+var user_count = 0;
+var turn_count = 0;
+
+io.socket.on('connection', function(socket){
+    
+    socket.on('join', function(data){
+        var username = data.username;
+        
+        socket.username = username;
+        
+        user[user_count] = {};
+        user[user_count].name = username;
+        user[user_count].turn = false;
+        
+        io.socket.emit('update_users', users);
+        
+        user_count++;
+    });//socket.on join -end
+    
+    socket.on('game_start', function(data){
+        socket.broadcast.emit("game_started", data);
+        user[turn_count].turn  = true;
+        
+        io.socket.emit('update_users', users);        
+    });//socket.on game_start - end
+    
+    socket.on('select', function(data){
+        socket.broadcast.emit("check_number", data);
+        ffdgfdg
+        users[turn_count].turn = false;
+        turn_count++;
+        if(turn_count >= user_count){
+            turn_count = 0;            
+        }
+        users[turn_count].turn = ture;
+        
+        io.socket.emit('update_users', users);
+    });//socket.on select - end
+    
+    socket.on('disconnect', function(){
+        delete user[socket.username];
+        io.socket.emit('update_users', users);
+        
+        user_count--;        
+    });//socket.on disconnect - end
+    
+    
+});//io.socket.on - end
+
+
 
 
--- rst_bingo/public/javascripts/main.js
+++ rst_bingo/public/javascripts/main.js
@@ -4,32 +4,87 @@ var bingo = {
         socket: null,
 
         init: function () {
-
-        },
+            var self = this;
+            
+            //initialize
+            this.is_my_turn = true;
+            
+            this.socket = io.connect('http://localhost:3000');
+            
+            this.socket.on("check_number",function(){
+                self.where_is_it(data.num);
+                self.print_msg(data.username + " checked '" +  data.num + "'");
+            });
+            
+            this.socket.on("game_started", function(data){
+                self.print_msg(data.username + " started this game ");
+                $("#start_button").hide;
+            });
+            
+            this.socket.on("update_users", function(data){
+                self.update_userlist(data);
+            });
+            
+            //join
+            this.socket.on("connect",function(){
+                self.socket.emit("join", { username: $("#username").val() });
+            });
+            
+            var numbers = [];
+            for(var i=1; i<=25; i++){
+                numbers.push(i);
+            }
+            
+            numbers.sort(function(a, b){
+                var temp = parseInt(Math.random() * 10);
+                var isOddOrEven = temp % 2;
+                var isPosOrNeg = temp > 5 ? 1 : -1;
+                return(isOddOrEven*isPosOrNeg);
+            });
+            
+            $("table.bingo-board td").each(function(){
+                $(this).html(numbers[i]);
+                
+                $(this).click(function(){
+                    self.select_num(this);
+                });
+            });
+            
+            $("#start_button").click(function(){
+                self.socket.emit("game_start",{ username  : $("username").val()});
+                self.print_msg("You started this game.");
+                $("#start_button").hide();
+            });            
+        },//init - end
+    
 
         select_num: function (obj){
+            if(this.is_my_turn && !$(obj).attr("checked")){
+                
+                
+            }
 
-        },
+        },//select_num - end
 
         where_is_it: function(num){
 
-        },
+        },//where_is_it - end
 
         chek_num: function(obj){
 
-        },
+        },//chek_num - end
 
         update_userlist: function(data){
 
-        },
+        },//update_userlist - end
 
         leave: function(){
 
-        },
+        },//leave - end
 
         print_msg: function(msg){
 
-        }
+        }//print_msg -end
 };
 
 $(document).ready(function(){

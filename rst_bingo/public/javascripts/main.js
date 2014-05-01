2037703266f0bc7058b7b01d5fe5cf635fd4bf9e
unknown <byeong_gyu@naver.com>
2014-05-01 11:59:53 +0900

    2014-05-01 am11:51 javascripts/main.js

--- rst_bingo/public/javascripts/main.js
+++ rst_bingo/public/javascripts/main.js
@@ -42,7 +42,7 @@ var bingo = {//main.jade 에서 bingo id값으로 이용
                 numbers.push(i);
                 console.log("numbers.push(i) = " + numbers.push(i));
             }
-            
+            console.log("numbers = " + numbers[]);
             //빙고 숫자 랜덤함수 이용 무작위로 숫자 생성
             numbers.sort(function(a, b){
                 var temp = parseInt(Math.random() * 10);
@@ -51,6 +51,7 @@ var bingo = {//main.jade 에서 bingo id값으로 이용
                 console.log("temp = " + temp);
                 console.log("isOddOrEven = " + isOddOrEven);
                 console.log("isPosOrNeg= " + isPosOrNeg);
+                console.log("isOddOrEven*isPosOrNeg " + isOddOrEven*isPosOrNeg);
                 return(isOddOrEven*isPosOrNeg);
             });
             
